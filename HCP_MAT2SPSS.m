15909336f01a29556d1d1375e5039ffd9a31b6bf
philjrdb <57160185+philjrdb@users.noreply.github.com>
2021-11-18 10:43:52 +1100

    Update HCP_MAT2SPSS.m

--- HCP_MAT2SPSS.m
+++ HCP_MAT2SPSS.m
@@ -1,6 +1,24 @@
 %% HCP Matlab-SPSS unravel
 % Puts HCPsum and questionnaire variables 
 
+%% By Philip Jean-Richard-dit-Bressel, UNSW Sydney, 2020
+% Feel free to use with citation: Jean-Richard-dit-Bressel, Lee et al. (2021). https://elifesciences.org/articles/69594
+
+%% GNU
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%% Experiment parameters:
 n_blocks = 6;
 pun_trials = 3:6;
 
